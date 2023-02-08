90614b4e0fd10868a7ab352a6bdf845c7936a5f9
erthbison <erthbison@outlook.com>
2023-02-08 09:47:18 +0100

    More description of Scheduler

--- gomc/scheduler.go
+++ gomc/scheduler.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Scheduler[T any] interface {
-	GetEvent() (Event[T], error) // Get the next event in the run
+	GetEvent() (Event[T], error) // Get the next event in the run. Will return RunEndedError if there are no more events in the run. Will return NoEventError if there are no more available events in any run.
 	AddEvent(Event[T])           // Add an event to the list of possible events
 	EndRun()                     // Finish the current run and prepare for the next one
 }
