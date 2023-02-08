ac335b9272294ba470e1b784a3022964b938901c
erthbison <erthbison@outlook.com>
2023-02-08 09:48:15 +0100

    Test for sleep manager

---
+++ gomc/test/sleepManager_test.go
@@ -0,0 +1,36 @@
+package gomc_test
+
+import (
+	"experimentation/gomc"
+	"testing"
+	"time"
+)
+
+func TestSleepManager(t *testing.T) {
+	sch := NewMockScheduler()
+	nextEvent := make(chan error)
+	sm := gomc.NewSleepManager[node](sch, nextEvent)
+	notBlockedChan := make(chan bool)
+	go func() {
+		// Need to check that sleep actually blocks until the event is executed
+		sm.Sleep(time.Second)
+		notBlockedChan <- true
+	}()
+
+	evt := <-sch.inEvent
+
+	// Wait until the nextEvent signal is received than execute the event.
+	// Like the simulator would
+	select {
+	case <-nextEvent:
+		evt.Execute(map[int]*node{}, nextEvent)
+	case <-notBlockedChan:
+		t.Fatalf("Sleep returned before the event was executed")
+	}
+	select {
+	case <-notBlockedChan:
+	case <-time.After(time.Second * 5):
+		t.Fatalf("Sleep is still blocking after executing the event")
+	}
+
+}
