3f77199961717ef392207a724b247859e32e1ccb
erthbison <erthbison@outlook.com>
2023-02-10 14:15:47 +0100

    Moved comments on Scheduler interface

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -6,9 +6,12 @@ import (
 )
 
 type Scheduler[T any] interface {
-	GetEvent() (event.Event[T], error) // Get the next event in the run. Will return RunEndedError if there are no more events in the run. Will return NoEventError if there are no more available events in any run.
-	AddEvent(event.Event[T])           // Add an event to the list of possible events
-	EndRun()                           // Finish the current run and prepare for the next one
+	// Get the next event in the run. Will return RunEndedError if there are no more events in the run. Will return NoEventError if there are no more available events in any run.
+	GetEvent() (event.Event[T], error)
+	// Add an event to the list of possible events
+	AddEvent(event.Event[T])
+	// Finish the current run and prepare for the next one
+	EndRun()
 }
 
 var (
