eb6557580feee0a43ed03ba8e1c6d543b0691463
erthbison <erthbison@outlook.com>
2023-02-10 14:28:09 +0100

    removed redundant properties and added more comments

--- simulator.go
+++ simulator.go
@@ -29,9 +29,6 @@ type Simulator[T any, S any] struct {
 
 	// Used to control the flow of events. The simulator will only proceed to gather state and run the next event after it receives a signal on the nextEvt chan
 	NextEvt chan error
-
-	end     bool
-	numRuns int
 }
 
 func NewSimulator[T any, S any](sch scheduler.Scheduler[T], sm StateManager[T, S]) *Simulator[T, S] {
@@ -39,13 +36,16 @@ func NewSimulator[T any, S any](sch scheduler.Scheduler[T], sm StateManager[T, S
 		Scheduler: sch,
 		sm:        sm,
 		NextEvt:   make(chan error),
-		numRuns:   0,
-		end:       false,
 	}
 }
 
+// Run the simulations of the algorithm.
+// initNodes: is a function that generates the nodes used and returns them in a map with the id as a key and the node as the value
+// funcs: is a variadic arguments of functions that will be scheduled as events by the scheduler. These are used to start the execution of the argument and can represent commands or requests to the service.
+// Simulate returns nil if the it runs to completion or reaches the max number of runs. It returns an error if it was unable to complete the simulation
 func (s Simulator[T, S]) Simulate(initNodes func() map[int]*T, funcs ...func(map[int]*T) error) error {
-	for s.numRuns < 50000 {
+	numRuns := 0
+	for numRuns < 50000 {
 		// Perform initialization of the run
 		nodes := initNodes()
 		s.sm.UpdateGlobalState(nodes)
@@ -65,15 +65,15 @@ func (s Simulator[T, S]) Simulate(initNodes func() map[int]*T, funcs ...func(map
 			// If there are no available events that means that all possible event chains have been attempted and we are done
 			return nil
 		} else if err != nil {
-			return fmt.Errorf("An error occurred while scheduling the next event: %w", err)
+			return fmt.Errorf("An error occurred while scheduling the next event: %v", err)
 		}
 		// End the run
 		// Add an end event at the end of this path of the event tree
 		s.Scheduler.EndRun()
 		s.sm.EndRun()
-		s.numRuns++
-		if s.numRuns%1000 == 0 {
-			fmt.Println("Running Simulation:", s.numRuns)
+		numRuns++
+		if numRuns%1000 == 0 {
+			fmt.Println("Running Simulation:", numRuns)
 		}
 	}
 	return nil
@@ -100,7 +100,7 @@ func (s *Simulator[T, S]) executeRun(nodes map[int]*T) error {
 		}()
 		err = <-s.NextEvt
 		if err != nil {
-			return fmt.Errorf("simulator: An error occurred while simulating: %w", err)
+			return fmt.Errorf("simulator: An error occurred while running the simulation: %v", err)
 		}
 		s.sm.UpdateGlobalState(nodes)
 	}
