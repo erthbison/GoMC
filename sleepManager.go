4633ba00ce2daf6ace43885626b1160fd384eb33
erthbison <erthbison@outlook.com>
2023-02-10 12:54:33 +0100

    changed sleepManagers method to be a factory where the id of the node is inputted

--- sleepManager.go
+++ sleepManager.go
@@ -22,14 +22,16 @@ func NewSleepManager[T any](sch scheduler.Scheduler[T], nextEvent chan error) *S
 	}
 }
 
-func (sm *SleepManager[T]) Sleep(_ time.Duration) {
-	_, file, line, _ := runtime.Caller(1)
-	evt := event.NewSleepEvent[T](fmt.Sprintf("File: %v, Line: %v", file, line), sm.sleepChans)
-	sm.sch.AddEvent(evt)
-	// Inform the simulator that the process is currently waiting for a scheduled timeout
-	// The simulator can now proceed with scheduling events
-	sm.nextEvt <- nil
+func (sm *SleepManager[T]) SleepFunc(id int) func(time.Duration) {
+	return func(_ time.Duration) {
+		_, file, line, _ := runtime.Caller(1)
+		evt := event.NewSleepEvent[T](fmt.Sprintf("File: %v, Line: %v", file, line), id, sm.sleepChans)
+		sm.sch.AddEvent(evt)
+		// Inform the simulator that the process is currently waiting for a scheduled timeout
+		// The simulator can now proceed with scheduling events
+		sm.nextEvt <- nil
 
-	// Wait until the event is executed before returning
-	<-sm.sleepChans[evt.Id()]
+		// Wait until the event is executed before returning
+		<-sm.sleepChans[evt.Id()]
+	}
 }
