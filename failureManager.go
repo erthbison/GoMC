ca76a95f28b647ec34035c79c6c5686e4261e718
erthbison <erthbison@outlook.com>
2023-02-14 13:57:43 +0100

    Comments to the failure detector

--- failureManager.go
+++ failureManager.go
@@ -1,5 +1,7 @@
 package gomc
 
+// The failureManager keeps track of which nodes has crashed and which has not.
+// It also provides a Subscribe() function which can be used to emulate the properties of a perfect failure detector
 type failureManager struct {
 	correct         map[int]bool
 	failureCallback []func(int)
@@ -12,16 +14,20 @@ func NewFailureManager() *failureManager {
 	}
 }
 
+// Init the failure manager with the provided nodes.
+// Set all the provided nodes to correct
 func (fm *failureManager) Init(nodes []int) {
 	for id := range nodes {
 		fm.correct[id] = true
 	}
 }
 
+// Return a map of the status of the nodes
 func (fm *failureManager) CorrectNodes() map[int]bool {
 	return fm.correct
 }
 
+// register that the node with the provided id has crashed
 func (fm *failureManager) NodeCrash(nodeId int) {
 	fm.correct[nodeId] = false
 	for _, f := range fm.failureCallback {
@@ -29,6 +35,7 @@ func (fm *failureManager) NodeCrash(nodeId int) {
 	}
 }
 
+// Register a callback function to be called when a node crashes.
 func (fm *failureManager) Subscribe(callback func(int)) {
 	fm.failureCallback = append(fm.failureCallback, callback)
 }
