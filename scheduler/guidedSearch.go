afd905687fd44bd4a821355a5138d7e47623490a
erthbison <erthbison@outlook.com>
2023-02-24 09:38:47 +0100

    Added a guidedSearch scheduler

---
+++ scheduler/guidedSearch.go
@@ -0,0 +1,72 @@
+package scheduler
+
+import (
+	"gomc/event"
+)
+
+// A scheduler that initially will follow a provided run before it begin searching the state space.
+type guidedSearch struct {
+	// The scheduler used to search the state space
+	search Scheduler
+
+	// The provided run
+	run    []string
+	guided *replayScheduler
+
+	useGuided bool
+}
+
+// Create a new GuidedSearch scheduler using the provided search scheduler for searching the state space after it hasa followed the provided run
+func NewGuidedSearch(search Scheduler, run []string) *guidedSearch {
+	guided := NewReplayScheduler(run)
+	return &guidedSearch{
+		search: search,
+
+		run:       run,
+		guided:    guided,
+		useGuided: true,
+	}
+}
+
+// Get the next event in the run. Will return RunEndedError if there are no more events in the run. Will return NoEventError if there are no more available events in any run.
+// The event returned must be an event that has been added during the current run.
+// Will follow the provided run until it has been completed or until it is unable to find the next event.
+// After that the scheduler will begin to search the state space using teh provided search scheduler.
+func (gs *guidedSearch) GetEvent() (event.Event, error) {
+	if gs.useGuided {
+		evt, err := gs.guided.GetEvent()
+		if err != nil {
+			gs.useGuided = false
+			// Migrate all pending events from the guided scheduler to the search scheduler
+			for _, evt := range gs.guided.pendingEvents {
+				gs.search.AddEvent(evt)
+			}
+			return gs.search.GetEvent()
+		}
+		return evt, err
+	} else {
+		return gs.search.GetEvent()
+	}
+}
+
+// Add an event to the list of possible events
+func (gs *guidedSearch) AddEvent(evt event.Event) {
+	if gs.useGuided {
+		gs.guided.AddEvent(evt)
+	} else {
+		gs.search.AddEvent(evt)
+	}
+}
+
+// Finish the current run and prepare for the next one
+func (gs *guidedSearch) EndRun() {
+	gs.useGuided = true
+	gs.guided = NewReplayScheduler(gs.run)
+	gs.search.EndRun()
+}
+
+// Signal to the scheduler that a node has crashed and that events targeting the node should not be scheduled
+func (gs *guidedSearch) NodeCrash(id int) {
+	gs.guided.NodeCrash(id)
+	gs.search.NodeCrash(id)
+}
---
+++ scheduler/guidedSearch_test.go
@@ -0,0 +1,93 @@
+package scheduler
+
+import (
+	"errors"
+	"testing"
+
+	"golang.org/x/exp/slices"
+)
+
+func TestGuidedSearch(t *testing.T) {
+	for i, test := range GuidedSearchTests {
+		sch := NewGuidedSearch(NewQueueScheduler(), test.run)
+
+		numRuns := 0
+		actualRun := []string{}
+		numEvent := 0
+		for {
+			if events, ok := test.events[numEvent]; ok {
+				// add some more events
+				for _, event := range events {
+					sch.AddEvent(event)
+				}
+			}
+			evt, err := sch.GetEvent()
+			if errors.Is(err, RunEndedError) {
+				sch.EndRun()
+				numRuns++
+				if !slices.Equal(test.run[:test.lengthReplay], actualRun[:test.lengthReplay]) {
+					t.Errorf("Did not follow run for start of execution in test %v", i)
+				}
+				actualRun = []string{}
+				numEvent = 0
+				continue
+			} else if errors.Is(err, NoEventError) {
+				break
+			}
+			actualRun = append(actualRun, evt.Id())
+			numEvent++
+		}
+		if numRuns != test.numRuns {
+			t.Errorf("Got an unexpected number of total runs in test %v. Got %v. Expected: %v", i, numRuns, test.numRuns)
+		}
+	}
+}
+
+var GuidedSearchTests = []struct {
+	run    []string
+	events map[int][]MockEvent
+	// Specify the number of events in the provided run that the scheduler is expected to follow before it begins searching
+	// This is generally the length of the run, but can be shorter if for instance the replayScheduler is unable to find the next event in the run
+	lengthReplay int
+	numRuns      int
+}{
+	{
+		run: []string{"1", "2", "3"},
+		events: map[int][]MockEvent{
+			0: {{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}},
+		},
+		lengthReplay: 3,
+		numRuns:      2,
+	},
+	{
+		run: []string{},
+		events: map[int][]MockEvent{
+			0: {{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}},
+		},
+		lengthReplay: 0,
+		numRuns:      120,
+	},
+	{
+		run: []string{"1", "2", "3", "4", "5"},
+		events: map[int][]MockEvent{
+			0: {{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}},
+		},
+		lengthReplay: 5,
+		numRuns:      1,
+	},
+	{
+		run:          []string{"1", "2", "3", "4", "5"},
+		events:       map[int][]MockEvent{0: {{id: 1}, {id: 2}, {id: 4}, {id: 5}}},
+		lengthReplay: 2,
+		numRuns:      2,
+	},
+	{
+		run: []string{"1", "2"},
+		events: map[int][]MockEvent{
+			0: {{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}},
+			2: {{id: 6}, {id: 7}},
+		},
+		lengthReplay: 2,
+		numRuns:      120,
+	},
+}
