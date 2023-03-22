b006c4ea4bde6e477db2db53dcb5025e80097c02
erthbison <erthbison@outlook.com>
2023-03-22 13:48:27 +0100

    queue/prefix scheduler is now concurrent

---
+++ scheduler/prefix.go
@@ -0,0 +1,166 @@
+package scheduler
+
+import (
+	"errors"
+	"gomc/event"
+	"sync"
+)
+
+type run []string
+
+type Prefix struct {
+	// Explores the state space by maintaining a stack of unexplored prefixes.
+	// When a new run is started it follows the prefix and begins exploring from there, adding new prefixes it discovers as it executes events
+
+	r []run
+
+	cond *sync.Cond
+
+	ongoing int
+}
+
+func NewPrefix() *Prefix {
+	ss := &Prefix{
+		r:    []run{{}},
+		cond: sync.NewCond(new(sync.Mutex)),
+	}
+	return ss
+}
+
+func (p *Prefix) GetRunScheduler() RunScheduler {
+	p.cond.L.Lock()
+	defer p.cond.L.Unlock()
+
+	p.ongoing++
+	return newRunQueue(p)
+}
+
+func (p *Prefix) addRun(r run) {
+	p.cond.L.Lock()
+	defer p.cond.L.Unlock()
+
+	p.r = append(p.r, r)
+
+	if len(p.r) == 1 {
+		p.cond.Broadcast()
+	}
+}
+
+func (p *Prefix) getRun() run {
+	p.cond.L.Lock()
+	defer p.cond.L.Unlock()
+
+	p.ongoing--
+	for len(p.r) == 0 {
+		if p.ongoing > 0 {
+			p.cond.Wait()
+		} else {
+			return nil
+		}
+	}
+
+	r := p.r[len(p.r)-1]
+	p.r = p.r[:len(p.r)-1]
+
+	p.ongoing++
+	return r
+}
+
+type runPrefix struct {
+	sync.Mutex
+
+	p *Prefix
+
+	currentIndex int
+	currentRun   run
+
+	pendingEvents []event.Event
+
+	pendingRuns []run
+}
+
+func newRunQueue(p *Prefix) *runPrefix {
+	return &runPrefix{
+		p: p,
+
+		currentIndex:  0,
+		currentRun:    make([]string, 0),
+		pendingEvents: make([]event.Event, 0),
+		pendingRuns:   make([]run, 0),
+	}
+}
+
+// Get the next event in the run. Will return RunEndedError if there are no more events in the run. Will return NoEventError if there are no more available events in any run.
+func (rp *runPrefix) GetEvent() (event.Event, error) {
+	rp.Lock()
+	defer rp.Unlock()
+
+	if len(rp.pendingEvents) == 0 {
+		return nil, RunEndedError
+	}
+
+	var evt event.Event
+	if rp.currentIndex < len(rp.currentRun) {
+		// Follow the current run until it has no more events
+		evtId := rp.currentRun[rp.currentIndex]
+		// Remove events from the pending events queue as they are selected
+		evt = rp.popEvent(evtId)
+		if evt == nil {
+			return nil, errors.New("Scheduler: Scheduled an event that was pending")
+		}
+	} else {
+		// Pop the last element from the pending events
+		evt = rp.pendingEvents[len(rp.pendingEvents)-1]
+		rp.pendingEvents = rp.pendingEvents[:len(rp.pendingEvents)-1]
+
+		// For all pending events we create a new run and add it to the pending runs queue
+		for _, pendingEvt := range rp.pendingEvents {
+			// Add these runs to the pending run slice
+			run := make([]string, len(rp.currentRun))
+			copy(run, rp.currentRun)
+			run = append(run, pendingEvt.Id())
+			rp.p.addRun(run)
+			// rss.pendingRuns = append(rss.pendingRuns, run)
+		}
+		rp.currentRun = append(rp.currentRun, evt.Id())
+	}
+	rp.currentIndex++
+	return evt, nil
+}
+
+func (rp *runPrefix) popEvent(evtId string) event.Event {
+	// Remove the message from the message queue
+	for i, pendingEvt := range rp.pendingEvents {
+		if evtId == pendingEvt.Id() {
+			rp.pendingEvents = append(rp.pendingEvents[:i], rp.pendingEvents[i+1:]...)
+			return pendingEvt
+		}
+	}
+	return nil
+}
+
+// Add an event to the list of possible events
+func (rp *runPrefix) AddEvent(evt event.Event) {
+	rp.Lock()
+	defer rp.Unlock()
+	rp.pendingEvents = append(rp.pendingEvents, evt)
+}
+
+func (rp *runPrefix) StartRun() error {
+	rp.Lock()
+	defer rp.Unlock()
+	// rss.sendRun <- rss.pendingRuns
+	rp.currentIndex = 0
+	r := rp.p.getRun()
+	if r == nil {
+		return NoEventError
+	}
+	rp.currentRun = r
+	return nil
+}
+
+// Finish the current run and prepare for the next one
+func (rss *runPrefix) EndRun() {
+	// qs.currentIndex = 0
+	// qs.currentRun = qs.ss.getRun()
+}
--- scheduler/prefix_test.go
+++ scheduler/prefix_test.go
@@ -3,19 +3,24 @@ package scheduler
 import "testing"
 
 func TestQueueSchedulerExplore2Events(t *testing.T) {
-	sch := NewQueueScheduler()
+	sch := NewPrefix()
 	testDeterministicExplore2Events(t, sch)
 }
 
 func TestQueueExploreBranchingEvents(t *testing.T) {
-	sch := NewQueueScheduler()
+	sch := NewPrefix()
 	testDeterministicExploreBranchingEvents(t, sch)
+}
+
+func TestConcurrentBranchingEvent(t *testing.T) {
+	sch := NewPrefix()
+	testConcurrentDeterministic(t, sch)
 }
 
 func BenchmarkQueueScheduler(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		sch := NewQueueScheduler()
-		err := benchmarkScheduler(sch, 7)
+		sch := NewPrefix()
+		err := benchmarkRunScheduler(sch.GetRunScheduler(), 7)
 		if err != nil {
 			b.Errorf(err.Error())
 		}
