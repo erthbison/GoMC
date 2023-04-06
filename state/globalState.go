dad689f15edb78e066473648a61576ac0a6f5001
erthbison <erthbison@outlook.com>
2023-04-06 11:36:10 +0200

    removed comment of old Evt property

--- state/globalState.go
+++ state/globalState.go
@@ -5,8 +5,7 @@ import "fmt"
 type GlobalState[S any] struct {
 	LocalStates map[int]S    // A map storing the local state of the nodes. The map stores (id, state) combination.
 	Correct     map[int]bool // A map storing the status of the node. The map stores (id, status) combination. If status is true, the node with id "id" is correct, otherwise the node is true. All nodes are represented in the map.
-	// Evt         event.Event  // A copy of the event that caused the transition into this state. It should not be changed.
-	Evt EventRecord // A record of the event that caused the transition into this state
+	Evt         EventRecord  // A record of the event that caused the transition into this state
 }
 
 func (gs GlobalState[S]) String() string {
