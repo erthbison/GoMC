a6b968b87de424667271d3a9e021c8aff855a501
erthbison <erthbison@outlook.com>
2023-04-06 11:32:07 +0200

    moved treeStateManager into own file

--- stateManager/stateManager.go
+++ stateManager/stateManager.go
@@ -1,13 +1,9 @@
 package stateManager
 
 import (
-	"fmt"
 	"gomc/state"
 	"gomc/tree"
-	"io"
 	"sync"
-
-	"golang.org/x/exp/maps"
 )
 
 // Manages the global state across several runs.
@@ -26,66 +22,4 @@ type TreeStateManager[T, S any] struct {
 
 	getLocalState func(*T) S
 	stateEq       func(S, S) bool
-}
-
-func NewTreeStateManager[T, S any](getLocalState func(*T) S, stateEq func(S, S) bool) *TreeStateManager[T, S] {
-	return &TreeStateManager[T, S]{
-		getLocalState: getLocalState,
-		stateEq:       stateEq,
-	}
-}
-
-func (sm *TreeStateManager[T, S]) AddRun(run []state.GlobalState[S]) {
-	sm.Lock()
-	defer sm.Unlock()
-	currentTree := sm.stateRoot
-	if len(run) < 1 {
-		return
-	}
-	if currentTree == nil {
-		currentTree = sm.initStateTree(run[0])
-		sm.stateRoot = currentTree
-	}
-	for _, state := range run[1:] {
-		// If the state already is a child of the current state, retrieve it and set it as the next state
-		if nextState := currentTree.GetChild(state); nextState != nil {
-			currentTree = nextState
-			continue
-		}
-		// Otherwise add it as a child to the state tree
-		currentTree = currentTree.AddChild(state)
-	}
-}
-
-// Initializes the state tree with the provided state as the initial state
-func (sm *TreeStateManager[T, S]) initStateTree(s state.GlobalState[S]) *tree.Tree[state.GlobalState[S]] {
-	cmp := func(a, b state.GlobalState[S]) bool {
-		if a.Evt.Id != b.Evt.Id {
-			return false
-		}
-		if !maps.EqualFunc(a.LocalStates, b.LocalStates, sm.stateEq) {
-			return false
-		}
-		return maps.Equal(a.Correct, b.Correct)
-	}
-	stateRoot := tree.New(s, cmp)
-	return stateRoot
-}
-
-// Create a RunStateManager to be used to collect the state of the new run
-func (sm *TreeStateManager[T, S]) GetRunStateManager() *RunStateManager[T, S] {
-	return NewRunStateManager[T, S](sm, sm.getLocalState)
-}
-
-// Write the Newick representation of the state tree to the writer
-func (sm *TreeStateManager[T, S]) Export(wrt io.Writer) {
-	sm.RLock()
-	defer sm.RUnlock()
-	fmt.Fprint(wrt, sm.stateRoot.Newick())
-}
-
-func (sm *TreeStateManager[T, S]) State() state.StateSpace[S] {
-	sm.RLock()
-	defer sm.RUnlock()
-	return state.TreeStateSpace[S]{Tree: sm.stateRoot}
 }
