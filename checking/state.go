f664cee0e23f2ff7d7966331807517e4cae39f39
erthbison <erthbison@outlook.com>
2023-04-06 11:36:20 +0200

    Added descriptive comments

--- checking/state.go
+++ checking/state.go
@@ -2,9 +2,10 @@ package checking
 
 import "gomc/state"
 
+// The state of the system at the current point of execution
 type State[S any] struct {
-	LocalStates map[int]S
-	Correct     map[int]bool
-	IsTerminal  bool
-	Sequence    []state.GlobalState[S]
+	LocalStates map[int]S              // The local states of the nodes.
+	Correct     map[int]bool           // The status of the nodes. True means that the node is correct, false that it has crashed.
+	IsTerminal  bool                   // True if this is the last recorded state in a run. False otherwise.
+	Sequence    []state.GlobalState[S] // The sequence of GlobalStates that lead to this State.
 }
