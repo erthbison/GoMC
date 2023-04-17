6351b3dbaacd61d801c964c67a2ff81d62723868
erthbison <erthbison@outlook.com>
2023-04-17 13:37:57 +0200

    moved treeStateManager to correct file

--- stateManager/stateManager.go
+++ stateManager/stateManager.go
@@ -2,8 +2,6 @@ package stateManager
 
 import (
 	"gomc/state"
-	"gomc/tree"
-	"sync"
 )
 
 // Manages the global state across several runs.
@@ -11,15 +9,4 @@ type StateManager[T, S any] interface {
 	GetRunStateManager() *RunStateManager[T, S]
 	AddRun(run []state.GlobalState[S])
 	State() state.StateSpace[S]
-}
-
-// A type that manages the state of several runs of the same system and represent all discovered states of the system.
-// The TreeStateManger waits for completed runs on the send channel. Once they are received they are added to the state tree that is used to represent the state space.
-// After calling Stop() the send channel will be closed and the TreeStateManager will no longer add new runs to the state tree
-type TreeStateManager[T, S any] struct {
-	sync.RWMutex
-	stateRoot *tree.Tree[state.GlobalState[S]]
-
-	getLocalState func(*T) S
-	stateEq       func(S, S) bool
 }
--- stateManager/treeStateManager.go
+++ stateManager/treeStateManager.go
@@ -5,9 +5,21 @@ import (
 	"gomc/state"
 	"gomc/tree"
 	"io"
+	"sync"
 
 	"golang.org/x/exp/maps"
 )
+
+// A type that manages the state of several runs of the same system and represent all discovered states of the system.
+// The TreeStateManger waits for completed runs on the send channel. Once they are received they are added to the state tree that is used to represent the state space.
+// After calling Stop() the send channel will be closed and the TreeStateManager will no longer add new runs to the state tree
+type TreeStateManager[T, S any] struct {
+	sync.RWMutex
+	stateRoot *tree.Tree[state.GlobalState[S]]
+
+	getLocalState func(*T) S
+	stateEq       func(S, S) bool
+}
 
 func NewTreeStateManager[T, S any](getLocalState func(*T) S, stateEq func(S, S) bool) *TreeStateManager[T, S] {
 	return &TreeStateManager[T, S]{
