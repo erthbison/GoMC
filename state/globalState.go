79e90afe50e79303b822dfa9173a748ae813d8d9
erthbison <erthbison@outlook.com>
2023-04-26 09:44:04 +0200

    added comments

--- state/globalState.go
+++ state/globalState.go
@@ -2,10 +2,16 @@ package state
 
 import "fmt"
 
+// The global state of the nodes at one time slot of the simulation
 type GlobalState[S any] struct {
-	LocalStates map[int]S    // A map storing the local state of the nodes. The map stores (id, state) combination.
-	Correct     map[int]bool // A map storing the status of the node. The map stores (id, status) combination. If status is true, the node with id "id" is correct, otherwise the node is true. All nodes are represented in the map.
-	Evt         EventRecord  // A record of the event that caused the transition into this state
+	// A map storing the local state of the nodes. The map stores (id, state) combination.
+	LocalStates map[int]S
+	// A map storing the status of the node. The map stores (id, status) combination.
+	// If status is true, the node with id "id" is correct, otherwise the node is true.
+	// All nodes are represented in the map.
+	Correct map[int]bool
+	// A record of the event that caused the transition into this state
+	Evt EventRecord
 }
 
 func (gs GlobalState[S]) String() string {
