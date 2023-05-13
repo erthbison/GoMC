bedb67aa1cde6385404411bc7e8fcd2d1300f5ff
erthbison <erthbison@outlook.com>
2023-05-13 11:49:06 +0200

    added comments to the Checker package

--- checking/checker.go
+++ checking/checker.go
@@ -5,11 +5,26 @@ import (
 	"gomc/state"
 )
 
+// The Checker verifies that properties hold for the state space.
 type Checker[S any] interface {
+	// Verify that the configured properties hold for the provided state space
 	Check(root state.StateSpace[S]) CheckerResponse
 }
 
+// CheckerResponse is a response returned by a Checker
+//
+// Contains the result of checking the system.
 type CheckerResponse interface {
+	// Create a response.
+	//
+	// Returns a boolean that is true if all properties hold, false otherwise.
+	// Returns a string describing the response.
+	// This should include a detailed description of which property is violated and the run which caused it to be violated.
 	Response() (bool, string)
+
+	// Export the run which caused a property to be violated
+	//
+	// If a property was violated it will return a slice containing the sequence of EventIds of the events in the run.
+	// Otherwise it will return an empty slice.
 	Export() []event.EventId
 }
--- checking/predicate.go
+++ checking/predicate.go
@@ -1,8 +1,10 @@
 package checking
 
 // Check that the predicate happens eventually.
-// This is done by only running the predicate on the terminalState of all sequences
-// For all non-terminal states the predicate always returns true
+//
+// Return a predicate that run the provided predicate on terminal states.
+// Returns the value of the original predicate if the state is terminal.
+// Otherwise, it always returns true.
 func Eventually[S any](pred Predicate[S]) Predicate[S] {
 	return func(s State[S]) bool {
 		if !s.IsTerminal {
@@ -12,7 +14,10 @@ func Eventually[S any](pred Predicate[S]) Predicate[S] {
 	}
 }
 
-// Check that the condition is true for all processes in the provided global state
+// Check that condition returns true for all processes in the provided global state
+//
+// Returns false if cond returns false for some node.
+// Returns true otherwise.
 // If checkCorrect is true, only correct processes will be checked, otherwise faulty processes will also be checked.
 func ForAllNodes[S any](cond func(S) bool, s State[S], checkCorrect bool) bool {
 	for id, state := range s.LocalStates {
