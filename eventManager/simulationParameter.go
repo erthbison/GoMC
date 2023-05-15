b10d54fb4740ae291d26d2e07a9fbe506ab4477d
erthbison <erthbison@outlook.com>
2023-05-15 14:22:40 +0200

    added comments about SimulationParameters

--- eventManager/simulationParameter.go
+++ eventManager/simulationParameter.go
@@ -1,7 +1,17 @@
 package eventManager
 
+// TODO: Update thesis to reflect the move of SimulationParameters into eventManager package
+
+// Stores the SimulationParameters used in the specific run of the simulation
+//
+// The parameters are used by to initialize the EventManager and to subscribe to the failure detector
 type SimulationParameters struct {
-	NextEvt        func(error, int)
+	// Signal the status of the executed event to the main loop.
+	NextEvt func(error, int)
+
+	// Used by node to subscribe to status updates from the failure detector
 	CrashSubscribe func(NodeId int, callback func(id int, status bool))
-	EventAdder     EventAdder
+
+	// Add events to the EventAdder used by the simulation
+	EventAdder EventAdder
 }
