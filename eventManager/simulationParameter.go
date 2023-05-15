6ee4834f359b77d780623ec9197345eba680168a
erthbison <erthbison@outlook.com>
2023-05-15 14:22:50 +0200

    spelling

--- eventManager/simulationParameter.go
+++ eventManager/simulationParameter.go
@@ -4,7 +4,7 @@ package eventManager
 
 // Stores the SimulationParameters used in the specific run of the simulation
 //
-// The parameters are used by to initialize the EventManager and to subscribe to the failure detector
+// The parameters are used to initialize the EventManager and to subscribe to the failure detector
 type SimulationParameters struct {
 	// Signal the status of the executed event to the main loop.
 	NextEvt func(error, int)
